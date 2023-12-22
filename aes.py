d9a1645f12e8d064cd16717952579a5faa5d01ca
Bastian Hagen <141170235+CookieMage@users.noreply.github.com>
2023-12-22 18:42:18 +0100

    basic aes functions

---
+++ aes.py
@@ -0,0 +1,30 @@
+from Crypto.Cipher import AES
+from random import randbytes
+
+def encrypt(key, plaintext):
+    key = b"16-byte"
+    cipher = AES.new(key, AES.MODE_EAX)
+
+    nonce = cipher.nonce
+    ciphertext, tag = cipher.encrypt_and_digest(plaintext)
+    return nonce, ciphertext, tag
+
+def decrypt(nonce, ciphertext, tag):
+    key = b'Sixteen byte key'
+    cipher = AES.new(key, AES.MODE_EAX, nonce=nonce)
+    plaintext = cipher.decrypt(ciphertext)
+    try:
+        cipher.verify(tag)
+        print("The message is authentic:", plaintext)
+    except ValueError:
+        print("Key incorrect or message corrupted")
+
+def main():
+    key = randbytes(16)
+    print(key)
+    message = encrypt(key, "hello")
+    print(message)
+    print(decrypt(message))
+
+if __name__ == "__main__":
+    main()
