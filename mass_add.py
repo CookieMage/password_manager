89502ca8c2d63f3e99bda49d8d1cfff749d805f2
Bastian Hagen <141170235+CookieMage@users.noreply.github.com>
2024-01-15 20:16:40 +0100

    done

--- mass_add.py
+++ mass_add.py
@@ -16,12 +16,19 @@ def read(file : str):
 def add_data(data : list, seed):
     for e in data:
         ctr = Counter.new(128)
-        length, encoded = manager.encode_data(e[0], e[0], e[0], e[0], seed, ctr)
+        length, encoded = manager.encode_data(e[0], e[1], e[2], e[3], seed, ctr)
         manager.add_data(encoded, length)
 
 def main():
-    file = input("file: ")
-    seed = getpass("seed: ")
+    seed = None
+    while not seed:
+        try:
+            seed = bytes(getpass("key: "), encoding="utf-8")
+        except ValueError:
+            print("Dies ist keine valide Eingabe. Ein Seed ist vom Typ bytes mit 16 Elementen.")
     
-    data = read(file)
+    data = read("passwords.txt")
     add_data(data, seed)
+
+if __name__ == "__main__":
+    main()
